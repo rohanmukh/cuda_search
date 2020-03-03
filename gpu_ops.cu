961f3d9391779d30337d3bef5c8e5a2d9dc80505
rm38 <rm38@rice.edu>
2020-03-03 00:40:49 -0600

    separated matrix vector operation to a new file

--- gpu_ops.cu
+++ gpu_ops.cu
@@ -4,36 +4,7 @@
 
 #include "gpu_ops.h"
 #include <cstdio>
-
-#define BLOCKSIZE 16
-
-
-/////////////////////////////////////////////////////////////////////////////////////////
-//
-// MatVect : this kernel will perform actual MatrixVector Multiplication
-//
-/////////////////////////////////////////////////////////////////////////////////////////
-__global__
-void MatVectMultiplication(const double *_device_Mat, const double *_device_Vect, int _matRowSize, int _vlength, double *_device_ResVect)
-{
-    int tidx = blockIdx.x*blockDim.x + threadIdx.x;
-    int tidy = blockIdx.y*blockDim.y + threadIdx.y;
-    int tindex=tidx+gridDim.x*BLOCKSIZE*tidy;
-
-
-    if(tindex < _matRowSize)
-    {
-        int i;int m= tindex * _vlength;
-        _device_ResVect[tindex]=0.00;
-        for(i=0; i < _vlength; i++)
-            _device_ResVect[tindex]+= _device_Mat[m + i] * _device_Vect[i];
-    }
-
-    __syncthreads();
-
-}//end of MatVect device function
-
-
+#include "matrix_vector_op.cu"
 
 /*function to launch kernel*/
 void gpu_ops::launch_kernel() {
