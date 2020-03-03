97461d3e4d49688431bdcd9a91fce5c5c19713e0
Rohan <rm38@rice.edu>
2020-03-02 19:50:38 -0600

    Adding list of gpus in manager

--- gpu_manager.cpp
+++ gpu_manager.cpp
@@ -12,19 +12,25 @@ int gpu_manager::get_device_count() {
     return count;
 }
 
+void gpu_manager::add_user(int device_id, int matRowSize, int matColSize, int vlength){
+    gpu_ops *gpu_user = new gpu_ops(matRowSize, matColSize, vlength);
+    gpu_user->set_device(0);
+    list_of_users.push_back(gpu_user);
+}
+
 void gpu_manager::copy_data(double* host_Mat, double* host_Vect) {
-    gpu_user->allocate_memory();
-    gpu_user->copy_to_device(host_Mat, host_Vect);
+    list_of_users.at(0)->allocate_memory();
+    list_of_users.at(0)->copy_to_device(host_Mat, host_Vect);
 }
 
 float gpu_manager::compute_and_store(double* host_ResVect) {
-    gpu_user->start_event();
-    gpu_user->launch_kernel();
-    gpu_user->copy_to_host(host_ResVect);
-    float time_sec = gpu_user->stop_event();
+    list_of_users.at(0)->start_event();
+    list_of_users.at(0)->launch_kernel();
+    list_of_users.at(0)->copy_to_host(host_ResVect);
+    float time_sec = list_of_users.at(0)->stop_event();
     return time_sec;
 }
 
 void gpu_manager::_free() {
-    gpu_user->_free();
+    list_of_users.at(0)->_free();
 }
--- gpu_manager.h
+++ gpu_manager.h
@@ -5,17 +5,17 @@
 #ifndef CUDA_CODE_SEARCH_GPU_MANAGER_H
 #define CUDA_CODE_SEARCH_GPU_MANAGER_H
 
+#include <vector>
 #include "gpu_ops.h"
 
 class gpu_manager {
-    gpu_ops *gpu_user;
+    std::vector<gpu_ops*> list_of_users;
 
 public:
-    gpu_manager(int device_id, int matRowSize, int matColSize, int vlength){
-        gpu_user = new gpu_ops(matRowSize, matColSize, vlength);
-        gpu_user->set_device(0);
+    gpu_manager(){
     }
 
+    void add_user(int, int, int, int);
     static int get_device_count();
     void copy_data(double*, double*);
     float compute_and_store(double*);
