55546075ec7f88b7df99eaaac276e40254f150b8
Rohan <rm38@rice.edu>
2020-03-02 20:11:32 -0600

    Adding list of gpus everywhere in manager

--- gpu_manager.cpp
+++ gpu_manager.cpp
@@ -14,23 +14,30 @@ int gpu_manager::get_device_count() {
 
 void gpu_manager::add_user(int device_id, int matRowSize, int matColSize, int vlength){
     gpu_ops *gpu_user = new gpu_ops(matRowSize, matColSize, vlength);
-    gpu_user->set_device(0);
+    gpu_user->set_device(device_id);
     list_of_users.push_back(gpu_user);
 }
 
 void gpu_manager::copy_data(double* host_Mat, double* host_Vect) {
-    list_of_users.at(0)->allocate_memory();
-    list_of_users.at(0)->copy_to_device(host_Mat, host_Vect);
+    for(int i=0; i<list_of_users.size(); i++){
+        list_of_users.at(i)->allocate_memory();
+        list_of_users.at(i)->copy_to_device(host_Mat, host_Vect);
+    }
 }
 
 float gpu_manager::compute_and_store(double* host_ResVect) {
-    list_of_users.at(0)->start_event();
-    list_of_users.at(0)->launch_kernel();
-    list_of_users.at(0)->copy_to_host(host_ResVect);
-    float time_sec = list_of_users.at(0)->stop_event();
+    float time_sec = 0.;
+    for(int i=0; i<list_of_users.size(); i++) {
+        list_of_users.at(i)->start_event();
+        list_of_users.at(i)->launch_kernel();
+        list_of_users.at(i)->copy_to_host(host_ResVect);
+        time_sec = std::max(time_sec, list_of_users.at(i)->stop_event());
+    }
     return time_sec;
 }
 
 void gpu_manager::_free() {
-    list_of_users.at(0)->_free();
+    for(int i=0; i<list_of_users.size(); i++) {
+        list_of_users.at(i)->_free();
+    }
 }
