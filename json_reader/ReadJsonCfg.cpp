f43bdd7113d350c442c34f3c76a899449f7e9285
Rohan <rm38@rice.edu>
2020-03-05 19:30:14 -0600

    Fixed a minor indexing bug

--- json_reader/ReadJsonCfg.cpp
+++ json_reader/ReadJsonCfg.cpp
@@ -17,17 +17,19 @@ int main()
 {
     int num_threads = NUM_THREADS;
     std::vector<ProgramBatch> list_of_batches;
+
     for (int i=0;i<num_threads;i++){
         auto batch1 = ProgramBatch(DATA_SIZE, DIMENSION);
         list_of_batches.push_back(batch1);
     }
+
     int offset = 0;
     for (int j=0;j<NUM_JSONS;j++) {
         int thread_id = j % num_threads;
         auto batch = list_of_batches.at(thread_id);
-        std::string file_name = "Program_output_" + std::to_string(j) + ".json";
+        std::string file_name = "Program_output_" + std::to_string(j+1) + ".json";
         batch.read_single_database_json(file_name);
-        std::cout << "JSON " << j << " has " << batch.num_programs << " elements." std::endl;
+        std::cout << "JSON " << j << " has " << batch.num_programs << " elements." << std::endl;
     }
 
 }
